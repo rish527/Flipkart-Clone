a6e7c9be33fd2844d5da5cf8472c09d497d41915
Asus <rishav032005@gmail.com>
2024-06-14 09:37:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 1 second
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Initial call to start the slideshow
+showSlides();
